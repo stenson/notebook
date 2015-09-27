0b005c1df9c31b66ac92e05d5ecc8267a2f4f70f
Rob Stenson <rob.stenson@gmail.com>
2015-09-27 15:23:39 -0700

    style and html

--- src/notebook/zs.clj
+++ src/notebook/zs.clj
@@ -3,7 +3,9 @@
             [puget.printer :refer [cprint]]
             [notebook.gdoc :as gdoc]
             [hiccup.core :as html]
-            [garden.core :as garden]))
+            [hiccup.page :refer [html5]]
+            [garden.core :as garden]
+            [garden.color :as color]))
 
 (def essay "14ANhea-S4bz9GOOOPIwCqfhKyGFpWQ2oQHtW1K-kHuQ")
 
@@ -11,6 +13,37 @@
   (let [html (gdoc/as-hiero-html essay)]
     (spit "wedding.html" (:html html))))
 
-(defn print-essay []
-  (let [content (slurp "wedding.html")]
-    (spit "index.html" content)))
+(defn favicon [size]
+  [:link {:href  (format "/favicon-%sx%s.png" size size)
+          :sizes (format "%sx%s" size size)
+          :rel   "icon"
+          :type  "image/png"}])
+
+(defn favicons [& sizes]
+  (->> (map favicon sizes)
+       (conj [:link {:href "/favicon.ico" :rel "icon" :type "image/x-icon"}])))
+
+(defn inline-style [& styles]
+  [:style {:type "text/css"}
+   (garden/css {:pretty-print? false} styles)])
+
+(let [content (slurp "wedding.html")]
+  (spit
+    "zhengstenson.com/index.html"
+    (html5
+      {:lang "en"}
+      (list
+        [:head
+         [:meta {:charset "utf-8"}]
+         [:title "Zheng Stenson Wedding"]
+         (list (favicons 16 32 96))
+         [:link {:type "text/css" :href "klim.css" :rel "stylesheet"}]
+         [:link {:type "text/css" :href "style.css" :rel "stylesheet"}]]
+        [:body
+         [:div#container
+          [:div#text-outer
+           [:div#text-inner
+            [:h1 "Diana &amp; Rob"]
+            [:h3 "Past to Present"]
+            [:div.content
+             content]]]]]))))
