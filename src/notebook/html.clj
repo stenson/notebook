b9f5dcf64adf959b72c118ce412155cb340ba4d6
Rob Stenson <rob.stenson@gmail.com>
2015-10-22 09:21:05 -0700

    better shorthand

---
+++ src/notebook/html.clj
@@ -0,0 +1,40 @@
+(ns notebook.html
+  (:require [garden.core :as garden]
+            [hiccup.page :refer [html5]]))
+
+(defn favicon [size]
+  [:link {:href  (format "favicon-%sx%s.png" size size)
+          :sizes (format "%sx%s" size size)
+          :rel   "icon"
+          :type  "image/png"}])
+
+(defn favicons [& sizes]
+  (->> (map favicon sizes)
+       (conj [:link {:href "favicon.ico" :rel "icon" :type "image/x-icon"}])))
+
+(defn inline-style [& styles]
+  [:style {:type "text/css"}
+   (garden/css {:pretty-print? false} styles)])
+
+(defn style-link [href]
+  [:link {:type "text/css"
+          :href (format "%s.css" href)
+          :rel "stylesheet"}])
+
+(defn js-link [href]
+  [:script {:type "text/javascript"
+            :src  (format "%s.js" href)}])
+
+(defn basic [title {:keys [styles scripts]} content]
+  (html5
+    {:lang "en"}
+    (list
+      [:head
+       [:meta {:charset "utf-8"}]
+       [:title title]
+       (list (favicons 16 32 96))
+       (list (map style-link styles))]
+      [:body
+       (list
+         content
+         (list (map js-link scripts)))])))
--- src/notebook/zs.clj
+++ src/notebook/zs.clj
@@ -2,51 +2,25 @@
   (:require [clojure.string :as string]
             [puget.printer :refer [cprint]]
             [notebook.gdoc :as gdoc]
-            [hiccup.core :as html]
             [hiccup.page :refer [html5]]
-            [garden.core :as garden]
-            [garden.color :as color]))
+            [notebook.html :as html]))
 
 (def essay "14ANhea-S4bz9GOOOPIwCqfhKyGFpWQ2oQHtW1K-kHuQ")
 
-(defn save-essay []
-  (let [html (gdoc/as-hiero-html essay)]
-    (spit "wedding.html" (:html html))))
-
-(defn favicon [size]
-  [:link {:href  (format "favicon-%sx%s.png" size size)
-          :sizes (format "%sx%s" size size)
-          :rel   "icon"
-          :type  "image/png"}])
-
-(defn favicons [& sizes]
-  (->> (map favicon sizes)
-       (conj [:link {:href "favicon.ico" :rel "icon" :type "image/x-icon"}])))
-
-(defn inline-style [& styles]
-  [:style {:type "text/css"}
-   (garden/css {:pretty-print? false} styles)])
-
-(let [;content (slurp "wedding.html")
-      content (:html (gdoc/as-hiero-html essay))]
+(defn build-site []
   (spit
-    "zhengstenson.com/index.html"
-    (html5
-      {:lang "en"}
-      (list
-        [:head
-         [:meta {:charset "utf-8"}]
-         [:title "Zheng Stenson Wedding"]
-         (list (favicons 16 32 96))
-         [:link {:type "text/css" :href "klim.css" :rel "stylesheet"}]
-         [:link {:type "text/css" :href "style.css" :rel "stylesheet"}]]
-        [:body
-         [:div#container
-          [:div#text-outer
-           [:div#text-inner
-            [:h1 "Diana &amp; Rob"]
-            [:img.sz {:src "sz-512.png" :width 32}]
-            [:div.content
-             content]]]]
-         [:script {:type "text/javascript"
-                   :src "hyphenator.js"}]]))))
+    "sites/zhengstenson.com/index.html"
+    (html/basic
+      "Zheng Stenson Wedding"
+      {:styles  ["klim"
+                 "style"]
+       :scripts ["hyphenator"]}
+      [:div#container
+       [:div#text-outer
+        [:div#text-inner
+         [:h1 "Diana &amp; Rob"]
+         [:img.sz {:src "sz-512.png" :width 32}]
+         [:div.content
+          (:html (gdoc/as-hiero-html essay))]]]])))
+
+(build-site)
