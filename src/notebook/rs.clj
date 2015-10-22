cd7060a0c89e8f6757b57dd62df4cb1f5703226a
Rob Stenson <rob.stenson@gmail.com>
2015-10-22 14:09:18 -0700

    pull in a gdoc

--- src/notebook/rs.clj
+++ src/notebook/rs.clj
@@ -1,5 +1,6 @@
 (ns notebook.rs
-  (:require [notebook.html :as html]))
+  (:require [notebook.html :as html]
+            [notebook.gdoc :as gdoc]))
 
 (defn essay [title subtitle link where about date]
   [:li
@@ -16,7 +17,7 @@
   "robstenson.com"
   "Rob Stenson"
   {:styles  ["styles" "fonts"]
-   :scripts []}
+   :scripts ["hyphenator"]}
   [:div#container
    [:div#outer-outer
     [:div#outer
@@ -25,7 +26,7 @@
        (essay "How to Get Hired at Twitter in 2011" nil
               "https://medium.com/@robstenson/how-to-get-hired-at-twitter-in-2011-22f03e8082ab#.a1hli53ft"
               "medium.com"
-              "Ruminations and useless advice from my stint in Silicon Valley"
+              "Ruminations and useless advice from a stint in Silicon Valley"
               "Aug 21, 2015")
        (essay "Lossy’s Uncharted Waters" nil
               "http://tonal.goodhertz.co/uncharted-waters/"
@@ -35,8 +36,13 @@
        (essay "Why I Play the Banjo" nil
               "https://medium.com/the-banjo/why-i-play-the-banjo-3e312da0eab7"
               "medium.com"
-              "A brief essay, mostly about my dad"
+              "(TL;DR my dad plays the banjo)"
               "Jan 30, 2015")
+       (essay "VULF0004 (Beastly 45\") Liner Notes" nil
+              "/vulf0004.jpg"
+              "vinyl record"
+              "Semi-fictional liner notes written in 45 minutes"
+              "Apr 4, 2013")
        (essay "Churches Hidden in Plain Text"
               "Mining and Mapping the Historiography of Gothic Architecture"
               "/pdfs/thesis.pdf"
@@ -51,4 +57,9 @@
               "Oct 25, 2007")]]]
     [:div#cameo
      [:div#cameo-inner]
-     [:img#rob {:src "me.png"}]]]])
+     [:img#rob {:src "me.png"}]]
+    [:div#about
+     (:html
+       (gdoc/parse
+         :txt
+         "13RSoNcQRUus9_13cy-2a3AQa_zJlFm39MvKu3YaoJvA"))]]])
