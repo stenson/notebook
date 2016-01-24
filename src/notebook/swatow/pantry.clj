4a7e7d5098ea9e404177e0349f9d03adef2c3528
Rob Stenson <rob.stenson@gmail.com>
2016-01-23 18:34:12 -0800

    new style for swatow.co

--- src/notebook/swatow/pantry.clj
+++ src/notebook/swatow/pantry.clj
@@ -3,20 +3,27 @@
             [notebook.gdoc :as gdoc]
             [puget.printer :as puget]))
 
-(def all-posts
-  (->> ["1uXDM4acSAxXTcYtwqoSmZK7XDxIWP7l40Ne6t9GAv9s"]
-       (map #(gdoc/parse :txt % false))
-       (doall)))
+(def all-posts (atom []))
+
+(defn get-all-posts []
+  (reset! all-posts
+          (->> ["1uXDM4acSAxXTcYtwqoSmZK7XDxIWP7l40Ne6t9GAv9s"]
+               (map #(gdoc/parse :txt % false))
+               (doall))))
+
+(get-all-posts)
 
 (defn page [{:keys [slug title]} html]
   (html/refresh
     (str "pantry.swatow.co" (if slug (str "/" slug)))
     title
-    {:styles ["/styles"]}
+    {:styles ["/styles"
+              "/fonts/fonts"
+              "/fonts/1139"]}
     [:div#container
      [:div#header-container
       [:div#header
-       [:h1 "The Swatow Pantry"]]]
+       [:img.logo {:src "/img/swatow-square.png" :alt "Swatow.co"}]]]
      html]))
 
 (defn post [p]
@@ -24,30 +31,22 @@
   (page
     p
     [:div.post-container
-     [:div.post
-      (:html p)]]))
+     [:div.post-container-inner
+      [:div.post
+       [:h1 (:title p)]
+       [:div.content
+        (:html p)]]]]))
 
 (do
   (page
-    {:title "The Swatow Pantry"}
+    {:title "Swatow.co"}
     [:div.posts-container
      [:div.posts
       [:ul
-       (for [{:keys [slug title lede]} all-posts]
-         [:li.post-link
-          [:a {:href (str "/" slug)}
-           [:h3 title]
-           [:h5 lede]]])]]])
-  (doall (map post all-posts)))
-
-#_(html/refresh
-  "pantry.swatow.co"
-  "The Swatow Pantry"
-  {:styles ["styles"]}
-  [:div#container
-   [:div#header-container
-    [:div#header
-     [:h1 "The Swatow Pantry"]]]
-   [:div.post-container
-    [:div.post
-     (:html dried-honey-dates)]]])
+       (for [{:keys [slug title lede]} @all-posts]
+         [:li.post-container
+          [:div.post-container-inner
+           [:a {:href (str "/" slug)}
+            [:h3 title]
+            [:h6 lede]]]])]]])
+  (doall (map post @all-posts)))
